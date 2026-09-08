f02f194f32e9ae9d5560b7a6126a9d61fdb40339
0-v-0 <kduice@qq.com>
2026-09-08 16:21:33 +0800

    feat: postgresql: DB_PGSQL native type mapping and packet output fixes
    Native type read/write under DB_PGSQL, alloca-free Output packet (DMD-safe), missing std.conv import in db.d.

--- database/postgresql/pool.d
+++ database/postgresql/pool.d
@@ -3,4 +3,5 @@ module database.postgresql.pool;
 import database.pool;
 import database.postgresql.connection;
 
+/++ Shared connection provider alias backed by `database.pool.ConnectionProvider`. +/
 alias ConnectionPool = shared ConnectionProvider!(Connection, 0);
